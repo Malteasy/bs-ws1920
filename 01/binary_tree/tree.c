4d13915195897cbbdddc9b80f7d9260de0b1ae1a
Malte Röder <malte.roeder@web.de>
2019-10-28 22:30:40 +0100

    2

--- 01/binary_tree/tree.c
+++ 01/binary_tree/tree.c
@@ -1,9 +1,9 @@
 #include "tree.h"
-node_t tree;
+node_t *tree;
 
 node_t *createTree(int rootValue){
-  tree.value = rootValue;
-  tree->parent = (int*)rootValue;
+  tree->value = rootValue;
+  tree->parent = NULL;
   tree->left = NULL;
   tree->right = NULL;
 
